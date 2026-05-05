441611f51f2b6fe552bbc7078ff5ca5e98582271
Ansh <anshgupta0412@gmail.com>
2026-05-06 03:24:33 +0530

    added connect-flash

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ app.use(cors({
 app.use(cookieParser("secretcode"));
 
 const sessionOptions = {
-  secret: "mysupersecretstring",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
